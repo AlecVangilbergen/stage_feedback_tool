502f005460e8ca971d411da17460f979cdcfd68e
AlecV <alec.vangilbergen@student.ucll.be>
2024-02-12 15:54:52 +0100

    Added navbar item

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -8,6 +8,7 @@ const Navigation: React.FC = () => {
         <li><a href="/">Home</a></li>
         {/* Add more navigation links here if needed */}
         <li><a href="/">Student Submission</a></li>
+        <li><a href="/">Student Final Submission</a></li>
         <li><a href="/">Student Analytics</a></li>
         <li><a href="/">Teacher Analytics</a></li>
         <li><a href="/">Admin Analytisc</a></li>
