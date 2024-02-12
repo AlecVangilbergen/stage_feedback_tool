be2dc498d13e56b98c72e70f0d818451e53747b6
AlecV <alec.vangilbergen@student.ucll.be>
2024-02-12 14:28:27 +0100

    Added initial html

---
+++ src/Navigation.tsx
@@ -0,0 +1,15 @@
+// src/Navigation.tsx
+import React from 'react';
+
+const Navigation: React.FC = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        {/* Add more navigation links here if needed */}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
